e3dc026ea5500a8a1642f13607d7078bd1458cfa
ioanpapas <johnie.papas@hotmail.com>
2021-07-29 11:52:56 +0300

    Added Test for Ascending Minima Class

--- softEng/src/test/java/evalTest/AppTest.java
+++ softEng/src/test/java/evalTest/AppTest.java
@@ -1,9 +1,14 @@
 package evalTest;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import org.checkerframework.checker.units.qual.A;
 import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -32,6 +37,10 @@ public class AppTest
 
     double[] testArray1={1,1,1};
     StatisticalUtilsArray arr1=new StatisticalUtilsArray(testArray1);
+
+    /**
+     * @link StatisticalUtilsArray
+     */
     @Test
     public void testStatsOfArray2(){
         assertEquals(1,arr1.findMinOfArray(),0.01);
@@ -40,6 +49,30 @@ public class AppTest
         assertEquals(1,arr1.findMedianOfArray(),0.01);
         assertEquals(0,arr1.findStDOfArray(),0.01);
     }
+
+    ArrayList<Double> arrayList=new ArrayList<Double>(){
+        {
+            add((double)5);
+            add((double)1);
+            add((double)3);
+            add((double)2);
+            add((double)6);
+            add((double)8);
+            add((double)4);
+            add((double)6);
+        }
+    };
+    int windows_size=3;
+    AscendingMinima ascMin=new AscendingMinima(windows_size, arrayList.size(),arrayList);
+    Double[] actual=ascMin.findAscendingMinima().toArray(new Double[arrayList.size()-windows_size+1]);
+    Double[] expected={(double)1,(double)1,(double)2,(double)2,(double)4,(double)4};
+    @Test
+    public void testAscendingMinima(){
+        assertArrayEquals(actual, expected);
+    }
+
+
+
 
     
 }
