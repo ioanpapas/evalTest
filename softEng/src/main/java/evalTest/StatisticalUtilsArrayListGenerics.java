036a76b48f61562c2e06c9dd9e0f92dd3d98909c
ioanpapas <johnie.papas@hotmail.com>
2021-08-03 10:44:08 +0300

    Added comments for new class

--- softEng/src/main/java/evalTest/StatisticalUtilsArrayListGenerics.java
+++ softEng/src/main/java/evalTest/StatisticalUtilsArrayListGenerics.java
@@ -3,20 +3,63 @@ package evalTest;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * This class implements 5 statistical utilities that are needed for an ArrayList of parameter T
+ * These are: Minimum, Maximum, Median, Mean Value and the Standard Deviation.
+ *
+ * @author Ioannis Papas
+ * @version 2021.1
+ * @since 02/08/2021
+ *
+ * @param <T> a generic parameter so the class is used for any numerical format (integer,float,double etc.)
+ */
+
 public class StatisticalUtilsArrayListGenerics<T extends Number>{
 
+    /**
+     * This is a method that finds the minimum value of the array list
+     *
+     * @param arrayList the arrayList used to find the minimum, with double values
+     * @param <T> a generic parameter so the array list can be of any numerical format
+     *
+     * @return The minimum value of the arrayList, as a double value
+     */
     public static <T extends Number> double findMinOfArrayList(ArrayList<T> arrayList){
         return arrayList.stream().map(v->v.doubleValue()).min(Comparator.naturalOrder()).orElseThrow(NoSuchElementException::new);
     }
 
+    /**
+     * This is a method that finds the maximum value of the array list
+     *
+     * @param arrayList the arrayList used to find the maximum, with double values
+     * @param <T> a generic parameter so the array list can be of any numerical format
+     *
+     * @return The maximum value of the arrayList, as a double value
+     */
     public static <T extends Number> double findMaxOfArrayList(ArrayList<T> arrayList){
         return arrayList.stream().map(v->v.doubleValue()).max(Comparator.naturalOrder()).orElseThrow(NoSuchElementException::new);
     }
 
+    /**
+     * This is a method that finds the mean value of the array list
+     *
+     * @param arrayList the arrayList used to find the mean, with T values
+     * @param <T> a generic parameter so the array list can be of any numerical format
+     *
+     * @return The maximum value of the arrayList, as a double value
+     */
     public static <T extends Number> double findMeanOfArrayList(ArrayList<T> arrayList){
         return arrayList.stream().map(v->v.doubleValue()).mapToDouble(v->v).sum()/arrayList.stream().count();
     }
 
+    /**
+     * This is a method that finds the minimum value of the array list
+     *
+     * @param arrayList the arrayList used to find the minimum, with double values
+     * @param <T> a generic parameter so the array list can be of any numerical format
+     *
+     * @return The minimum value of the arrayList, as a double value
+     */
     public static double findMedianOfArrayList(ArrayList<Double> arrayList) {
         if (arrayList.size() == 0) {
             System.out.println("ArrayList is empty");
@@ -29,6 +72,14 @@ public class StatisticalUtilsArrayListGenerics<T extends Number>{
             return (double) sorted.get(sorted.size() / 2);
     }
 
+    /**
+     * This is a method that finds the minimum value of the array list
+     *
+     * @param arrayList the arrayList used to find the minimum, with double values
+     * @param <T> a generic parameter so the array list can be of any numerical format
+     *
+     * @return The minimum value of the arrayList, as a double value
+     */
     public static double findStDOfArrayList(ArrayList<Double> arrayList){
         if(arrayList.size()==0){
             System.out.println("ArrayList is empty");
